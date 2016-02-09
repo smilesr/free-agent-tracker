40393b5d77be9154737004eb776a66bd60fa5d65
Robert Smiles Jr <smilesr120@gmail.com>
2016-02-09 11:22:41 -0500

    scraper code

--- agent_scraper.rb
+++ agent_scraper.rb
@@ -1,3 +1,23 @@
 require 'httparty'
 require 'nokogiri'
+
+
+page = HTTParty.get('http://espn.go.com/mlb/freeagents')
+
+parse_page = Nokogiri::HTML(page)
+
+rows = parse_page.css(".tablehead tr:gt(3)")
+
+details = rows.collect do |row|
+  detail = {}
+  detail1 = []
+  detail2 = {}
+ 
+  detail2["old_team"] = row.css('td[5]/text()').to_s 
+  detail2["new_team"] = row.css('td[6]/text()').to_s
+  detail1 << detail2
+
+  detail[(row.css('a/text()').to_s)] = detail1
+  detail
+  end
 end
