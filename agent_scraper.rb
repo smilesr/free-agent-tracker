f22b48ba719ccc6aa36558d105ee89820c58483a
Robert Smiles Jr <smilesr120@gmail.com>
2016-02-09 11:57:58 -0500

    parsed ESPN data into new hash

--- agent_scraper.rb
+++ agent_scraper.rb
@@ -21,3 +21,27 @@ details = rows.collect do |row|
   detail
   end
 end
+current_team = Hash.new{|h,k| h[k] = []}
+details.each do |item|
+  item.each do |key, value|
+    value.each do |team|
+      if team["new_team"] !~ /[A-Z][a-z]+/
+        if key == ""
+          next
+        else
+          # puts "#{key} has not been signed yet"
+          current_team[key] << team["old_team"]
+          current_team[key] << "unsigned" 
+        end
+      elsif team["old_team"] == team["new_team"]
+        # puts "#{key} has not changed teams and is still with #{team["old_team"]}"
+        current_team[key] << team["old_team"]
+        current_team[key] << "old"       
+      else
+        # puts "#{key} has changed teams from #{team["old_team"]} to #{team["new_team"]}"
+        current_team[key] << team["new_team"]
+        current_team[key] << "new"
+      end
+    end
+  end
+end 
